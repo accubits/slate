73806e4e8b7ea046c1f8d49ef200ea733b1063d3
Rahul AR <rahular@Rahuls-MacBook-Pro.local>
2019-11-28 15:01:16 +0530

    Deamon start for docker added

--- server.sh
+++ server.sh
@@ -2,4 +2,4 @@
 
 root=`pwd`/`dirname $0`
 docker build -t apidoc $root/docker
-docker run -p 4567:4567 -v $root/source:/slate/source -v $root/build:/slate/build apidoc
+docker run -d -p 4567:4567 -v $root/source:/slate/source -v $root/build:/slate/build apidoc
